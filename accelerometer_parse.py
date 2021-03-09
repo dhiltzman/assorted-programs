62a4d64d341e14051175998b1a0f8bcd0dd6ba81
David Hiltzman <69487089+dhiltzman@users.noreply.github.com>
2021-03-08 22:17:33 -0600

    Add files via upload

--- accelerometer_parse.py
+++ accelerometer_parse.py
@@ -8,7 +8,8 @@ import math
 the_desktop = os.path.join('C:\\Users',getpass.getuser(),'Desktop')
 os.chdir(the_desktop)
 
-with open('accelerometer_data.csv', 'w') as f:
+print('Compiling A0.')
+with open('AccelA0_data.csv', 'w') as f:
     f.write('Accelerometer,X-Accel,Y-Accel,Z-Accel,Magnitude\n')
 
 with open('sensors.txt', 'r') as f:
@@ -18,20 +19,131 @@ with open('sensors.txt', 'r') as f:
         
         #Accelerometer Title
         split1 = line.split(':')
-        #print(split1)
         accel_type = split1[0].strip()
 
-        split2 = split1[1].split(',')
-        x_accel = split2[0].strip()
-        x_squared = float(x_accel) ** 2
+        if (accel_type == 'A0'):
+            split2 = split1[1].split(',')
+            x_accel = split2[0].strip()
+            x_squared = float(x_accel) ** 2
         
-        y_accel = split2[1].strip()
-        y_squared = float(y_accel) ** 2
+            y_accel = split2[1].strip()
+            y_squared = float(y_accel) ** 2
         
-        z_accel = split2[2].strip()
-        z_squared = float(z_accel) ** 2
-        #print(split2)
+            z_accel = split2[2].strip()
+            z_squared = float(z_accel) ** 2
 
-        #Computing the Magnitude
-        magnitude = math.sqrt(float(x_squared) + float(y_squared) + float(z_squared))
-        #print(magnitude)
+            #Computing the Magnitude
+            magnitude = math.sqrt(float(x_squared) + float(y_squared) + float(z_squared))
+
+            full_data = accel_type + ',' + x_accel + ',' + y_accel + ',' + z_accel + ',' + str(magnitude) + '\n'
+        
+            with open('AccelA0_data.csv', 'a') as f:
+                f.write(full_data)
+    
+print('A0 Complete!')
+
+#A1
+print('Compiling A1.')
+with open('AccelA1_data.csv', 'w') as f:
+    f.write('Accelerometer,X-Accel,Y-Accel,Z-Accel,Magnitude\n')
+
+with open('sensors.txt', 'r') as f:
+    #A0: -5.34,-5.99,5.58,4908
+    for line in f:
+        #print('Inside For Loop')
+        
+        #Accelerometer Title
+        split1 = line.split(':')
+        accel_type = split1[0].strip()
+
+        if (accel_type == 'A1'):
+            split2 = split1[1].split(',')
+            x_accel = split2[0].strip()
+            x_squared = float(x_accel) ** 2
+        
+            y_accel = split2[1].strip()
+            y_squared = float(y_accel) ** 2
+        
+            z_accel = split2[2].strip()
+            z_squared = float(z_accel) ** 2
+
+            #Computing the Magnitude
+            magnitude = math.sqrt(float(x_squared) + float(y_squared) + float(z_squared))
+
+            full_data = accel_type + ',' + x_accel + ',' + y_accel + ',' + z_accel + ',' + str(magnitude) + '\n'
+        
+            with open('AccelA1_data.csv', 'a') as f:
+                f.write(full_data)
+    
+print('A1 Complete!')
+
+#A2
+print('Compiling A2.')
+with open('AccelA2_data.csv', 'w') as f:
+    f.write('Accelerometer,X-Accel,Y-Accel,Z-Accel,Magnitude\n')
+
+with open('sensors.txt', 'r') as f:
+    #A0: -5.34,-5.99,5.58,4908
+    for line in f:
+        #print('Inside For Loop')
+        
+        #Accelerometer Title
+        split1 = line.split(':')
+        accel_type = split1[0].strip()
+
+        if (accel_type == 'A2'):
+            split2 = split1[1].split(',')
+            x_accel = split2[0].strip()
+            x_squared = float(x_accel) ** 2
+        
+            y_accel = split2[1].strip()
+            y_squared = float(y_accel) ** 2
+        
+            z_accel = split2[2].strip()
+            z_squared = float(z_accel) ** 2
+
+            #Computing the Magnitude
+            magnitude = math.sqrt(float(x_squared) + float(y_squared) + float(z_squared))
+
+            full_data = accel_type + ',' + x_accel + ',' + y_accel + ',' + z_accel + ',' + str(magnitude) + '\n'
+        
+            with open('AccelA2_data.csv', 'a') as f:
+                f.write(full_data)
+    
+print('A2 Complete!')
+
+#A3
+print('Compiling A3.')
+with open('AccelA3_data.csv', 'w') as f:
+    f.write('Accelerometer,X-Accel,Y-Accel,Z-Accel,Magnitude\n')
+
+with open('sensors.txt', 'r') as f:
+    #A0: -5.34,-5.99,5.58,4908
+    for line in f:
+        #print('Inside For Loop')
+        
+        #Accelerometer Title
+        split1 = line.split(':')
+        accel_type = split1[0].strip()
+
+        if (accel_type == 'A3'):
+            split2 = split1[1].split(',')
+            x_accel = split2[0].strip()
+            x_squared = float(x_accel) ** 2
+        
+            y_accel = split2[1].strip()
+            y_squared = float(y_accel) ** 2
+        
+            z_accel = split2[2].strip()
+            z_squared = float(z_accel) ** 2
+
+            #Computing the Magnitude
+            magnitude = math.sqrt(float(x_squared) + float(y_squared) + float(z_squared))
+
+            full_data = accel_type + ',' + x_accel + ',' + y_accel + ',' + z_accel + ',' + str(magnitude) + '\n'
+        
+            with open('AccelA3_data.csv', 'a') as f:
+                f.write(full_data)
+    
+print('A3 Complete!')
+
